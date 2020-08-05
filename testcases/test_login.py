1a6317cc6aaef8e3986524ed0afa3c8f9e1abb5f
sharly2012 <sharly2012@sina.com>
2020-08-05 10:05:26 +0800

    improve the logger and fix the code

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -1,13 +1,13 @@
 import pytest
 from pages.loginpage import LoginPage
-from common.baseutil import get_yaml_value
+from common.baseutil import get_config_value
 
 
 class TestLogin(object):
 
     def test_login(self, driver):
         login_page = LoginPage(driver)
-        login_page.login(get_yaml_value('MOATS', 'mobile'), get_yaml_value('MOATS', 'password'))
+        login_page.login(get_config_value('MOATS', 'mobile'), get_config_value('MOATS', 'password'))
         title = login_page.get_title()
         assert title == '仟寻移动招聘-招聘管理平台'
 
